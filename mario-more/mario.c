e9e1fb9dc55a7cf5683b0b61d17174fd4337731e
Pei <pswu@protonmail.com>
2023-02-17 18:21:06 +0000

    Fri, Feb 17, 2023, 7:21 PM +01:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -16,13 +16,24 @@ void print_pyramid(int height)
     for (int i = 0; i < height; i++)
     {
         // left half of pyramid
-        for (int j = 0; j <= i; j++)
+        for (int j = height - 1; j > i; j--)
         {
-            printf("#");
+            printf(" ");
         }
         for (int j = 0; j <= i; j++)
         {
             printf("#");
+        }
+        // the gap
+        printf("  ");
+        // right half of pyramid
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        for (int j = height - 1; j > i; j--)
+        {
+            printf(" ");
         }
         printf("\n");
     }
@@ -35,6 +46,6 @@ int get_height(void)
     {
         height = get_int("What is the height?\n");
     }
-    while (height <= 0); // the height should be greater than 0
+    while (height <= 0 || height > 8); // the height should be greater than 0
     return height;
 }
