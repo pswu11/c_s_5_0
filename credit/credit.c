205edb46882097c06fe0c7806f186b504b1130e6
Pei <pswu@protonmail.com>
2023-02-17 18:43:35 +0000

    Fri, Feb 17, 2023, 7:43 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,10 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(void)
 {
-
+    
 }
 
 string validate(string card_num)
@@ -14,5 +15,8 @@ string validate(string card_num)
 
 string process(string card_num)
 {
-
+    for (int i = 0; i < strlen(card_num); i++)
+    {
+        printf(cardnum[i]);
+    }
 }
