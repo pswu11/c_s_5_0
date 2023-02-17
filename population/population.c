a15132d95c85284bc021e6f9be7efe7124600913
Pei <pswu@protonmail.com>
2023-02-17 16:47:17 +0000

    Fri, Feb 17, 2023, 5:47 PM +01:00

--- population/population.c
+++ population/population.c
@@ -4,10 +4,13 @@
 int main(void)
 {
     // TODO: Prompt for start size
-
+    int start_size = get_input("Start size:\n");
+    printf("Start size: %i"\n);
     // TODO: Prompt for end size
-
+    int start_size = get_input("Start size:\n");
+    printf("Start size: %i"\n);
     // TODO: Calculate number of years until we reach threshold
-
+    int start_size = get_input("Start size:\n");
+    printf("Start size: %i"\n);
     // TODO: Print number of years
 }
