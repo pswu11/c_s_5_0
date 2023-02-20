c78ff7f0ea79e558057199eedd97577ad0c9c1bf
Pei <pswu@protonmail.com>
2023-02-20 17:02:24 +0000

    Mon, Feb 20, 2023, 6:02 PM +01:00

--- wk2/hello.c
+++ wk2/hello.c
@@ -8,13 +8,17 @@ int main(void)
 }
 
 
-// to use 3rd party library like cs50.h
+// 3rd party Lib:
+// To use 3rd party library like cs50.h
 // it has to be installed under /usr/include/...
 // make is not a compiler, it uses "clang"
 // the command line would be: "clang -lcs50 -o hello hello.c"
 // "-lcs50 links the cs50 library and include the library"
 
-// 4 steps that compiler does:
+// .c file: your application
+// .h file: a library that can be reused
+
+// What a compiler does:
 // 1. preprocess (i.e. include necessary libraries) ->
 // 2. compile (from C to Assembly code) =>
 // assembly code is what our intel or arm cpu actually understands
@@ -22,7 +26,9 @@ int main(void)
 // 4. link (linking the machine codes from hello.c, stdio.h, cs50.h)
 
 
-// .c file: your application
-// .h file: a library that can be reused
+// Decompiling: reverse the process of compiling
+// Is it possible? Yes, but not so obvious and it's not as easy as imagine.
+// - variable names are not reversed
+// - for loop and while loop might look different
 
-// decompiling: reverse the process of compiling
+// Debugging:
