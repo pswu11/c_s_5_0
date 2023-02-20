c299c7790d9592ae718e16bbdae1125cdbe1705c
Pei <pswu@protonmail.com>
2023-02-20 12:56:45 +0000

    Mon, Feb 20, 2023, 1:56 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -10,23 +10,15 @@ int main(void)
     get_card();
 }
 
-void process(string card_num)
+string validate(long card_num)
 {
-    char *str = card_num;
-    size_t length = strlen(str);
 
-    for (size_t i = 0; i < length; i++)
-    {
-        if (isdigit(str[i]) == true) // this needs to be rewrite.
-        {
-            printf("%c\n", str[i]);
-        }
-    }
 }
 
 long get_card(void)
 {
     long card_num = get_long("Input your card number: ");
     printf("%li\n", card_num);
-    return card_num;
+    string val = validate(card_num);
+    return val;
 }
