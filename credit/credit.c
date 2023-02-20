c18d8db4660bd66f3ece707e6d25a9fb9baaed4c
Pei <pswu@protonmail.com>
2023-02-20 14:35:21 +0000

    Mon, Feb 20, 2023, 3:35 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -77,7 +77,7 @@ bool checksum(long card_num)
         {
             digit = card_num % 10;
             card_num = card_num / 10;
-            printf("%i\n", digit);
+            printf("%i\n", digit * 2);
             sum1 += digit * 2;
         }
         else
