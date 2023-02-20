c3cf1b686612f30613e064d511067fd766c8d8eb
Pei <pswu@protonmail.com>
2023-02-20 18:45:59 +0000

    Mon, Feb 20, 2023, 7:45 PM +01:00

--- wk2/greet.c
+++ wk2/greet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(int argc, string argv[])
+int main(int argc, string argv[]) // taking a user input as argument.
 {
-    string name = get_string("What's your name?\n"); 
+    string name = get_string("What's your name?\n");
     printf("hello, %s\n", name);
     printf("hello, %s\n", argv[1]);
 }
