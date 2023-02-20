b016d634d71cbab412d65aacb9e5f499058fcdf7
Pei <pswu@protonmail.com>
2023-02-20 19:35:23 +0000

    Mon, Feb 20, 2023, 8:35 PM +01:00

--- wk2/greet.c
+++ wk2/greet.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(int argc, string argv[]) // taking a user input as argument.
+int main(int argc, string argv[]) // taking a user input as arguments.
 {
-    string name = get_string("What's your name?\n");
-    printf("hello, %s\n", name);
-    printf("hello, %s\n", argv[1]);
+    // string name = get_string("What's your name?\n");
+    // printf("hello, %s\n", name);
+    // printf("hello, %s\n", argv[1]);
     // why 1 here?
     // it turns out that when we send "./greet Pei"
     // "./greet" => argv[0] ; "Pei" => argv[1]
     printf("The length is: %i\n", argc);
+
+    if (argc <= 2)
+    {
+        printf("hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("hello, world!\n"); // default if only one argument was given
+    }
 }
