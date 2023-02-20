be197fb63cbb2764194fd0b3b4267dc07761bee9
Pei <pswu@protonmail.com>
2023-02-20 16:54:15 +0000

    Mon, Feb 20, 2023, 5:54 PM +01:00

--- wk2/hello.c
+++ wk2/hello.c
@@ -6,3 +6,16 @@ int main(void)
     string name = get_string("What's your name? \n");
     printf("hello, %s\n", name);
 }
+
+
+// to use 3rd party library like cs50.h
+// it has to be installed under /usr/include/...
+// make is not a compiler, it uses "clang"
+// the command line would be: "clang -lcs50 -o hello hello.c"
+// "-lcs50 links the cs50 library and include the library"
+
+// 4 steps that compiler does:
+// 1. preprocess (i.e. include necessary libraries) ->
+// 2. compile (from C to Assembly code) =>
+// assembly code is what our intel or arm cpu actually understands
+// 3. assemble (convert from assembly to machine code) =>
