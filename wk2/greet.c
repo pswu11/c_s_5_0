e97b38e3037060f693c70b482fbbe4a689b11eaa
Pei <pswu@protonmail.com>
2023-02-20 18:43:54 +0000

    Mon, Feb 20, 2023, 7:43 PM +01:00

--- wk2/greet.c
+++ wk2/greet.c
@@ -4,5 +4,6 @@
 int main(int argc, string argv[])
 {
     string name = get_string("What's your name?");
-    printf("hello, #s\n", name);
+    printf("hello, %s\n", name);
+    printf("hello, %s\n", argv[1]);
 }
