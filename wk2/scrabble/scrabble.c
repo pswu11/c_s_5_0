075e2a66fb40099e7d707afc28e16595c4f60601
Pei <pswu@protonmail.com>
2023-02-20 21:17:02 +0000

    Mon, Feb 20, 2023, 10:17 PM +01:00

--- wk2/scrabble/scrabble.c
+++ wk2/scrabble/scrabble.c
@@ -41,15 +41,12 @@ int main(void)
 int compute_score(string word)
 {
     int score = 0;
+    // should I use switch here?
     for (int i = 0; i < strlen(word); i++)
     {
         char c;
-        int point;
-        if (isalpha(c))
-        {
-            point = 0;
-        }
-        else if (isupper(word[i]))
+        int point = 0;
+        if (isupper(word[i]))
         {
             c = tolower(word[i]);
         }
