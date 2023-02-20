3d8f8e38efbe936994ea22b475faf7a47878cd71
Pei <pswu@protonmail.com>
2023-02-20 20:52:57 +0000

    Mon, Feb 20, 2023, 9:52 PM +01:00

--- wk2/scrabble/scrabble.c
+++ wk2/scrabble/scrabble.c
@@ -7,7 +7,7 @@
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 // Create an array for alphabets
-char ALPHABETS[] = "abcdefghijklmnopqrstuvwxyz";
+char *string = "abcdefghijklmnopqrstuvwxyz";
 
 int compute_score(string word);
 string convert_lower(string word);
@@ -44,7 +44,7 @@ int compute_score(string word)
     // should I use switch here?
     for (int i = 0; i < strlen(word); i++)
     {
-        char c;
+        char *c;
         if (isupper(word[i]))
         {
             c = tolower(word[i]);
