af9daa1d4e6cb945e0d61543a8d3202f6787619d
Pei <pswu@protonmail.com>
2023-02-20 21:06:22 +0000

    Mon, Feb 20, 2023, 10:06 PM +01:00

--- wk2/scrabble/scrabble.c
+++ wk2/scrabble/scrabble.c
@@ -22,7 +22,7 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    // Print the winner
     string convert_lower(string word);
     if (score1 > score2)
     {
@@ -38,6 +38,7 @@ int main(void)
     }
 }
 
+// Compute the scores
 int compute_score(string word)
 {
     int score = 0;
@@ -60,6 +61,7 @@ int compute_score(string word)
     return score;
 }
 
+// Return the index of char in the alphabets string
 int get_index(char c)
 {
     int i = 0;
