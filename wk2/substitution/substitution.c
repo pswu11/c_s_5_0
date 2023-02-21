2035b4417a72019595f96d866c153b79bd18d207
Pei <pswu@protonmail.com>
2023-02-21 17:44:34 +0000

    Tue, Feb 21, 2023, 6:44 PM +01:00

--- wk2/substitution/substitution.c
+++ wk2/substitution/substitution.c
@@ -27,16 +27,19 @@ string convert(string txt, string sub)
         if(!isalpha(txt[i]))
         {
             result[i] = txt[i];
+            printf("%i: %c\n", i, txt[i]);
         }
         else
         {
             if (isupper(txt[i]))
             {
                 result[i] = sub[txt[i] - 'A'];
+                printf("%i: %c\n", i, txt[i]);
             }
             else if (islower(txt[i]))
             {
                 result[i] = sub[txt[i] - 'a'];
+                printf("%i: %c\n", i, txt[i]);
             }
         }
     }
