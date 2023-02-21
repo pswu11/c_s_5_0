26a01ec5ad2b6a63d8f34b8b2d6c683379ac1bc3
Pei <pswu@protonmail.com>
2023-02-21 19:46:32 +0000

    Tue, Feb 21, 2023, 8:46 PM +01:00

--- wk2/substitution/substitution.c
+++ wk2/substitution/substitution.c
@@ -13,12 +13,12 @@ int main(int argc, string argv[])
         return 1;
     }
     string sub = argv[1];
-    if (!validate_key(sub))
+    if (validate_key(sub))
     {
-        return 1;
+        string input = get_string("plaintext: ");
+        convert(input, sub);
     }
-    string input = get_string("plaintext: ");
-    convert(input, sub);
+
 }
 
 string convert(string txt, string sub)
