981fad5eb806557dc973c8acec9c2269b12166e0
Pei <pswu@protonmail.com>
2023-02-21 12:14:56 +0000

    Tue, Feb 21, 2023, 1:14 PM +01:00

--- wk2/readability/readability.c
+++ wk2/readability/readability.c
@@ -20,7 +20,7 @@ int main(void)
     float S = sentence_count / (float) word_count * 100;
     printf("L: %f \nS: %f \n", L, S);
     float index = 0.0588 * L - 0.296 * S - 15.8;
-    printf("Score: %f", index);
+    printf("Score: %f\n", index);
 }
 
 int count_letters(string t)
