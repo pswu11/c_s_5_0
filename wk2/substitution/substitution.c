c788bee04079a43cd5ab0f7fe60a961e05226375
Pei <pswu@protonmail.com>
2023-02-21 13:04:07 +0000

    Tue, Feb 21, 2023, 2:04 PM +01:00

--- wk2/substitution/substitution.c
+++ wk2/substitution/substitution.c
@@ -1,7 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    string sub = argv[1];
+    string input = get_string("plaintext: ");
+}
 
+string convert(string txt, string sub)
+{
+    for (int i = 0; i < strlen(txt); i++)
+    {
+        if (isupper(txt[i]))
+        {
+            txt[i] 
+        }
+    }
 }
