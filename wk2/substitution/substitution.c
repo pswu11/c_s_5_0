323202227f72a8f38861f20d7f57bb7e06faadd1
Pei <pswu@protonmail.com>
2023-02-21 13:23:00 +0000

    Tue, Feb 21, 2023, 2:23 PM +01:00

--- wk2/substitution/substitution.c
+++ wk2/substitution/substitution.c
@@ -17,12 +17,11 @@ string convert(string txt, string sub)
         if (isupper(txt[i]))
         {
             printf("%c\n", sub[txt[i] - 'A']);
-            strcat(result, sub[txt[i] - 'A']);
         }
         else if (islower(txt[i]))
         {
             //strcat(result, sub[txt[i] - 'a']);
         }
     }
-    return result;
+    return "what";
 }
