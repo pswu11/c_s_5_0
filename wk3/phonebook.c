4d0f96aa3650a7a553630b1952035db0a343af52
Pei <pswu@protonmail.com>
2023-02-22 10:27:07 +0000

    Wed, Feb 22, 2023, 11:27 AM +01:00

--- wk3/phonebook.c
+++ wk3/phonebook.c
@@ -8,9 +8,11 @@ int main(void)
     // if you want to have both name and numbers
     // get another array or use 2D array.
     string numbers[] = {"+49-152-38965479", "+886-911-333335"};
+    // Design problem: you have to maintain two arrays and make sure
+    // the index is not screwed up.
 
     string input = get_string("Name: ");
-    for (int i = 0; i < 1; i++)
+    for (int i = 0; i < 2; i++)
     {
         if (strcmp(names[i], input) == 0)
         {
@@ -22,3 +24,4 @@ int main(void)
     return 1;
 
 }
+
---
+++ wk3/phonebook2.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Custom data structure: define a new struct type
+typedef struct
+{
+    string name;
+    string number;
+}
+person; // This is the name of the customed struct type
+
+int main(void)
+{
+    person contacts[2];
+    contacts[0].name = "Carter";
+    contacts[0].number = "+49-152-38965479";
+    contacts[1].name = "David";
+    contacts[1].number = "+886-911-333335";
+
+    string input = get_string("Name: ");
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(contacts[i].name, input) == 0)
+        {
+            printf("Found %s: %s\n", contacts[i].name, contacts[i].number);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
+
