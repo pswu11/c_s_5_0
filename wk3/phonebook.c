6a16eb3cfe8834c29d8588205de5293df42eb479
Pei <pswu@protonmail.com>
2023-02-22 10:18:09 +0000

    Wed, Feb 22, 2023, 11:18 AM +01:00

--- wk3/phonebook.c
+++ wk3/phonebook.c
@@ -5,5 +5,20 @@
 int main(void)
 {
     string names[] = {"Carter", "David"};
+    // if you want to have both name and numbers
+    // get another array or use 2D array.
+    string numbers[] = {"+49-152-38965479", "+886-911-333335"};
+
+    string input = get_string("Name: ");
+    for (int i = 0; i < 1; i++)
+    {
+        if (strcmp(names[i], input) == 0)
+        {
+            printf("Found %s: %s\n", names[i], numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 
 }
