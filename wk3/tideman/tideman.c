facab310e7d27d1b5538e143cef4f0a7462d6915
Pei <pswu@protonmail.com>
2023-02-23 18:35:47 +0000

    Thu, Feb 23, 2023, 7:35 PM +01:00

--- wk3/tideman/tideman.c
+++ wk3/tideman/tideman.c
@@ -158,6 +158,7 @@ void sort_pairs(void)
     }
     for (int i = 0; i < pair_count; i++)
     {
+        // Find max value in pairs.
         int max_index = i;
         for (int j = i + 1; i < pair_count - 1; j++)
         {
