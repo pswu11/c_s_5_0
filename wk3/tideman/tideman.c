a9b3951a82ae0749fc627a4272f5b1176eb70a9d
Pei <pswu@protonmail.com>
2023-02-23 17:21:51 +0000

    Thu, Feb 23, 2023, 6:21 PM +01:00

--- wk3/tideman/tideman.c
+++ wk3/tideman/tideman.c
@@ -101,7 +101,11 @@ bool vote(int rank, string name, int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-
+        if (strcmp(candidates[i], name) == 0)
+        {
+            ranks[rank] = name;
+            return true;
+        }
     }
     return false;
 }
