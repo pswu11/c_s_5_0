99e778fc30f538a578f2e664066de16da743ac09
Pei <pswu@protonmail.com>
2023-02-23 19:08:48 +0000

    Thu, Feb 23, 2023, 8:08 PM +01:00

--- wk3/tideman/tideman.c
+++ wk3/tideman/tideman.c
@@ -130,8 +130,10 @@ void add_pairs(void)
     pair_count = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = i + 1; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
+            if (i == j)
+
             if (preferences[i][j] > preferences[j][i])
             {
                 pairs[pair_count].winner = i;
