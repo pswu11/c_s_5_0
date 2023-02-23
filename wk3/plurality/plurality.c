f2ce42b8b49b8c64697b89f0c5227e31592ebbb9
Pei <pswu@protonmail.com>
2023-02-23 16:29:35 +0000

    Thu, Feb 23, 2023, 5:29 PM +01:00

--- wk3/plurality/plurality.c
+++ wk3/plurality/plurality.c
@@ -70,7 +70,7 @@ bool vote(string name)
     {
         if (strcmp(name, candidates[i].name) == 0)
         {
-            candidates[i].name += 1;
+            candidates[i].votes += 1;
             return true;
         }
     }
