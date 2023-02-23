887fb0def81079e105d5fce644c3a106bf73b2b8
Pei <pswu@protonmail.com>
2023-02-23 18:47:45 +0000

    Thu, Feb 23, 2023, 7:47 PM +01:00

--- wk3/tideman/tideman.c
+++ wk3/tideman/tideman.c
@@ -151,7 +151,7 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    int vic[];
+    int vic[pair_count];
     for (int i = 0; i < pair_count; i++)
     {
         vic[i] = preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].loser][pairs[i].winner]
