58bdb47c186cd43933d39a68621eba3189920de8
Pei <pswu@protonmail.com>
2023-02-24 13:01:37 +0000

    Fri, Feb 24, 2023, 2:01 PM +01:00

--- wk3/tideman/tideman.c
+++ wk3/tideman/tideman.c
@@ -180,14 +180,12 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    for (int i = 0; i < pair_count; i++)
-    {
-        if ()
-        {
 
-        }
+    do
+    {
         locked[pairs[i].winnder][pairs[i].loser] = true;
-    }
+        i++;
+    } while (pairs[i].loser = pairs[i-2].winner)
     return;
 }
 
