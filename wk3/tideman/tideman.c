0711bb8fde7273c667b720d3edf272696550dc98
Pei <pswu@protonmail.com>
2023-02-24 21:51:47 +0000

    Fri, Feb 24, 2023, 10:51 PM +01:00

--- wk3/tideman/tideman.c
+++ wk3/tideman/tideman.c
@@ -203,10 +203,6 @@ void lock_pairs(void)
 // Print the winner of the election
 void print_winner(void)
 {
-    for (int i = 0; i < candidate_count; i++)
-    {
-        check_cycle()
-    }
     return;
 }
 
