43b7bec422d24c999f87f89aef313f66148e19f7
Pei <pswu@protonmail.com>
2023-02-24 23:56:30 +0000

    Sat, Feb 25, 2023, 12:56 AM +01:00

--- wk3/tideman/tideman.c
+++ wk3/tideman/tideman.c
@@ -210,7 +210,10 @@ void print_winner(void)
         for (int j = 0; j < candidate_count; j++)
         {
             c += locked[j][i];
-            printf("%i", c);
+        }
+        if (c == 0)
+        {
+            printf("%s", candidates[j]);
         }
     }
 }
