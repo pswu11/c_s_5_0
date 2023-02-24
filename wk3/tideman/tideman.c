9356a09f402c1356c2c97fca5e66b587a58fc37a
Pei <pswu@protonmail.com>
2023-02-24 21:15:13 +0000

    Fri, Feb 24, 2023, 10:15 PM +01:00

--- wk3/tideman/tideman.c
+++ wk3/tideman/tideman.c
@@ -202,6 +202,10 @@ void print_winner(void)
 // Check whether there's a cycle
 bool check_cycle(int start, int end)
 {
+    if (start == end)
+    {
+        return true;
+    }
     for (int i = 0; i < pair_count; i++)
     {
         if (locked[end][i])
