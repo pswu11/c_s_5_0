bea002b6ae074b9acbe1109f8fc4a8cd36a15488
Pei <pswu@protonmail.com>
2023-02-28 18:05:57 +0000

    Tue, Feb 28, 2023, 7:05 PM +01:00

--- wk4/copy.c
+++ wk4/copy.c
@@ -8,8 +8,14 @@ int main(void)
     string s = get_string("s: ");
 
     string t = s;
+    // above line assigns t with memory address of s;
+    // hence both t and s points to the same address.
 
-    t[0] = toupper(t[0]);
+    // avoid segmentation fault in case users give empty values
+    if (str(len) > 0)
+    {
+            t[0] = toupper(t[0]);
+    }
 
     printf("s: %s\n", s);
     printf("t: %s\n", t);
