096f573c78bf6d0e41470792c75e9faccaef176e
Pei <pswu@protonmail.com>
2023-02-28 18:24:12 +0000

    Tue, Feb 28, 2023, 7:24 PM +01:00

--- wk4/copy.c
+++ wk4/copy.c
@@ -8,6 +8,8 @@ int main(void)
 {
     char *s = get_string("s: ");
 
+    // Make a COPY #1
+
     char *t = s; // not the right way to make a copy of t
     // above line assigns t with memory address of s;
     // hence both t and s points to the same address.
@@ -21,12 +23,18 @@ int main(void)
     printf("s: %s\n", s);
     printf("t: %s\n", t);
 
+    // Make a COPY #2 => not recommended cuz things could still go wrong.
+    // i.e. get_string will return NULL if something goes wrong.
+    // i.e. alloc will retrun NULL if it's asking too much memory.
 
     // add one more memory slot longer than s
     char *t2 = malloc(strlen(s) + 1);
     // correct way to make a copy of s
     // +1 for having space for \0
-    for (int i = 0; i < strlen(s) + 1; i++)
+
+    // optimized for loops by making strlen(s) a varialbe so the loop doesn't
+    // call the same function again and again.
+    for (int i = 0, n = strlen(s) + 1; i < n; i++)
     {
         t2[i] = s[i];
     }
@@ -38,6 +46,22 @@ int main(void)
 
     printf("s: %s\n", s);
     printf("t2: %s\n", t2);
+
+    // it's a good habit to return the memory after it's used.
+    free(t2);
+
+    // Make a COPY #3
+
+    strcpy(t3, s);
+
+    if (strlen(t3) > 0)
+    {
+            t3[0] = toupper(t3[0]);
+    }
+
+    printf("s: %s\n", s);
+    printf("t3: %s\n", t3);
+
 }
 
 // malloc: ask your OS to allocate free memory
