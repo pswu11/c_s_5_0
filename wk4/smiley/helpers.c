091e1d84b724a4f9ec8931a1d6f707464ddeb8b8
Pei <pswu@protonmail.com>
2023-03-02 17:46:04 +0000

    Thu, Mar 2, 2023, 6:46 PM +01:00

--- wk4/smiley/helpers.c
+++ wk4/smiley/helpers.c
@@ -8,12 +8,13 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            // create a pointer to where the image array is at in the memory
             RGBTRIPLE *pixel = &image[i][j];
             if (pixel->rgbtBlue == 255)
             {
-                pixel->rgbtBlue = 115;
-                pixel->rgbtGreen = 56;
-                pixel->rgbtRed = 138;
+                pixel->rgbtBlue = 230;
+                pixel->rgbtGreen = 80;
+                pixel->rgbtRed = 160;
             }
         }
     }
