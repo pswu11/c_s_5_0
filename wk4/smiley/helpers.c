842d02561dceeca934e680a9eedcf36e6c33115a
Pei <pswu@protonmail.com>
2023-03-02 15:44:39 +0000

    Thu, Mar 2, 2023, 4:44 PM +01:00

--- wk4/smiley/helpers.c
+++ wk4/smiley/helpers.c
@@ -6,9 +6,11 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            if (image[i][j].rgbtBlue == 255 && image[i][j].rgbtBlue == 255 && image[i][j].rgbtBlue == 255)
+            if (image[i][j].rgbtBlue == 255 && image[i][j].rgbtGreen == 255 && image[i][j].rgbtRed == 255)
             {
-                
+                image[i][j].rgbtBlue = 115;
+                image[i][j].rgbtGreen = 56;
+                image[i][j].rgbtRed = 138;
             }
         }
     }
