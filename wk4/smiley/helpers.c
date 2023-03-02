be1ae4a5e6e8c7ec8f1697341f4cd31e8037d4dd
Pei <pswu@protonmail.com>
2023-03-02 17:50:51 +0000

    Thu, Mar 2, 2023, 6:50 PM +01:00

--- wk4/smiley/helpers.c
+++ wk4/smiley/helpers.c
@@ -1,16 +1,17 @@
 #include "helpers.h"
 #include "stdio.h"
 
+// color any black pixel to purple
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    printf("height: %i, width: %i\n", height, width);
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            // create a pointer to where the image array is at in the memory
+            // Create a pointer to where the image array is at in the memory
             RGBTRIPLE *pixel = &image[i][j];
-            if (pixel->rgbtBlue == 255)
+            // Color the pixel if the pixel is black (then a single color code can't be 0x00)
+            if (pixel->rgbtRed != 0x00)
             {
                 pixel->rgbtBlue = 230;
                 pixel->rgbtGreen = 80;
