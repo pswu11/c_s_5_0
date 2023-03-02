e0fae3f29aaf200f06de8f5f1fe5300d4031407d
Pei <pswu@protonmail.com>
2023-03-02 15:53:32 +0000

    Thu, Mar 2, 2023, 4:53 PM +01:00

--- wk4/smiley/helpers.c
+++ wk4/smiley/helpers.c
@@ -2,12 +2,12 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE pixel = image[0][0];
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            if (image[i][j].rgbtBlue == 255 && image[i][j].rgbtGreen == 255 && image[i][j].rgbtRed == 255)
+            RGBTRIPLE pixel = image[i][j];
+            if (pixel.rgbtBlue == 255 && pixel.rgbtGreen == 255 && pixel.rgbtRed == 255)
             {
                 image[i][j].rgbtBlue = 115;
                 image[i][j].rgbtGreen = 56;
