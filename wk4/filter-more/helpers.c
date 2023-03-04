e9b8c20c10cfbc4c0ba45d7b7078fd5812033de3
Pei <pswu@protonmail.com>
2023-03-04 16:47:24 +0000

    Sat, Mar 4, 2023, 5:47 PM +01:00

--- wk4/filter-more/helpers.c
+++ wk4/filter-more/helpers.c
@@ -21,6 +21,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE *pixel = &image[i][j];
+            int avg = round((pixel->rgbtBlue + pixel->rgbtRed + pixel->rgbtGreen) / (float) 3);
+            pixel->rgbtBlue = avg;
+            pixel->rgbtRed = avg;
+            pixel->rgbtGreen = avg;
+        }
+    }
     return;
 }
 
