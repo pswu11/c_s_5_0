4fa380727eab9534456f4745bff8b183f3b0debe
Pei <pswu@protonmail.com>
2023-03-04 17:21:39 +0000

    Sat, Mar 4, 2023, 6:21 PM +01:00

--- wk4/filter-more/helpers.c
+++ wk4/filter-more/helpers.c
@@ -42,6 +42,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            for (int k = -1; i < 2; k++)
+            {
+                
+            }
+        }
+    }
     return;
 }
 
