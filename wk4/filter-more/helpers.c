31cd82fb557809e744f2fa41b0df09a05cfa37ae
Pei <pswu@protonmail.com>
2023-03-04 17:15:32 +0000

    Sat, Mar 4, 2023, 6:15 PM +01:00

--- wk4/filter-more/helpers.c
+++ wk4/filter-more/helpers.c
@@ -25,7 +25,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE temp = &image[i][width - 1 - j];
+            RGBTRIPLE temp[j] = image[i][width - 1 - j];
         }
     }
     return;
