61ca545ed9df3e4ddacaffd8c086a257b030788a
Pei <pswu@protonmail.com>
2023-03-05 14:47:35 +0000

    Sun, Mar 5, 2023, 3:47 PM +01:00

--- wk4/reverse/reverse.c
+++ wk4/reverse/reverse.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
     fread(&bf, sizeof(WAVHEADER), 1, file);
 
     // Use check_format to ensure WAV format
-    if (bf[8])
+    printf("%s\n", bf[]);
 
     // Open output file for writing
     // TODO #5
