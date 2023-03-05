2f76e2f8acba5ddb18f3f5ec160e1b246df68609
Pei <pswu@protonmail.com>
2023-03-05 11:52:33 +0000

    Sun, Mar 5, 2023, 12:52 PM +01:00

--- wk4/filter-more/helpers.c
+++ wk4/filter-more/helpers.c
@@ -43,7 +43,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE *pixel = &image[height][width];
     // Make a copy
     RGBTRIPLE temp[height][width];
     for (int i = 0; i < height; i++)
@@ -59,8 +58,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            RGBTRIPLE *pixel = &image[i][j];
             // Calculate each pixel's 3x3 average
-            float sum_red = 0, sum_green = 0,sum_blue = 0;
+            int sum_red = 0, sum_green = 0,sum_blue = 0;
             int counter = 0;
             for (int a = -1; a < 2; a++)
             {
@@ -70,16 +70,16 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     int y = j + b;
                     if (x >= 0 && x < height && y >= 0 && y < width)
                     {
+                        sum_red += temp[x][y].rgbtRed;
+                        sum_green += temp[x][y].rgbtGreen;
+                        sum_blue += temp[x][y].rgbtBlue;
+                        counter++;
                     }
-                    sum.rgbtRed += temp[x][y].rgbtRed;
-                    sum.rgbtGreen += temp[x][y].rgbtGreen;
-                    sum.rgbtBlue += temp[x][y].rgbtBlue;
-                    counter++;
                 }
             }
-            pixel->rgbtRed = sum.rgbtRed / counter;
-            pixel->rgbtGreen = sum.rgbtGreen / counter;
-            pixel->rgbtBlue = sum.rgbtBlue / counter;
+            pixel->rgbtRed = sum_red / counter;
+            pixel->rgbtGreen = sum_green / counter;
+            pixel->rgbtBlue = sum_blue / counter;
         }
     }
     return;
