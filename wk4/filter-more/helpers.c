46aa1386c4fe9e4fe6865c470a5e063a821abfce
Pei <pswu@protonmail.com>
2023-03-05 11:54:31 +0000

    Sun, Mar 5, 2023, 12:54 PM +01:00

--- wk4/filter-more/helpers.c
+++ wk4/filter-more/helpers.c
@@ -52,7 +52,6 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             temp[i][j] = image[i][j];
         }
     }
-    printf("first pixel: (%i, %i, %i)\n", temp[0][0].rgbtRed, temp[0][0].rgbtGreen, temp[0][0].rgbtBlue);
     // Iterate over each pixel
     for (int i = 0; i < height; i++)
     {
