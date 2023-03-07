45f748e469c380c8914bceccf8b0c4bf1b3760a9
Pei <pswu@protonmail.com>
2023-03-07 10:35:35 +0000

    Tue, Mar 7, 2023, 11:35 AM +01:00

--- wk5/node.c
+++ wk5/node.c
@@ -3,9 +3,11 @@
 // initialize node to NULL to make sure there's no garbage value there.
 node *list = NULL;
 node *n = malloc(sizeof(node));
-node *n = malloc(sizeof(node));
 
 // (*n).number = 1;
 n->number = 1;
-n->next =
+n->next = NULL; // to signify this is the end of the list
 
+list = n; // this allows list to point to first node directly
+
+node *n = malloc(sizeof(node));
