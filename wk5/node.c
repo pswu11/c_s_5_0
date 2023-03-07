fbcbf5542ebd00f2f1f82a90476f2c618abb07b8
Pei <pswu@protonmail.com>
2023-03-07 10:33:21 +0000

    Tue, Mar 7, 2023, 11:33 AM +01:00

--- wk5/node.c
+++ wk5/node.c
@@ -3,3 +3,9 @@
 // initialize node to NULL to make sure there's no garbage value there.
 node *list = NULL;
 node *n = malloc(sizeof(node));
+node *n = malloc(sizeof(node));
+
+// (*n).number = 1;
+n->number = 1;
+n->next =
+
