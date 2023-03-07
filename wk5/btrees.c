94297ab79fd94ebe0fb0895fb0194a88099efd1b
Pei <pswu@protonmail.com>
2023-03-07 17:59:34 +0000

    Tue, Mar 7, 2023, 6:59 PM +01:00

--- wk5/btrees.c
+++ wk5/btrees.c
@@ -14,3 +14,26 @@ int main(int argc, char *argv[])
 {
     node *list = NULL;
 }
+
+// Search for a value in a binary tree using recursion
+// Downside: memory consumption (a lot more)
+// Upside:
+bool search(node *tree, int number)
+{
+    if (tree == NULL)
+    {
+        return false;
+    }
+    if (number < tree->number)
+    {
+        return search(tree->left, number);
+    }
+    else if (number > tree->number)
+    {
+        return search(tree->right, number);
+    }
+    else
+    {
+        return true;
+    }
+}
