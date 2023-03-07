93f606e359ea822f5ca1c87553f65ab6fef82d89
Pei <pswu@protonmail.com>
2023-03-07 10:42:01 +0000

    Tue, Mar 7, 2023, 11:42 AM +01:00

--- wk5/node.c
+++ wk5/node.c
@@ -15,3 +15,5 @@ list = n;
 node *n = malloc(sizeof(node));
 n->number = 2;
 // list = n;  this will cause memory leak.
+n->next = list; // now node 2 is pointing to 1
+list = n; // now you have a linked list with order in 2, 1, which is like a Stack.
