7a71ec8925be51714e8a746ecde1ae123cd2017d
Pei <pswu@protonmail.com>
2023-03-07 17:55:29 +0000

    Tue, Mar 7, 2023, 6:55 PM +01:00

---
+++ wk5/btrees.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// implement single linked lists
+typedef struct node
+{
+    int number;
+    struct node *left;
+    struct node *right;
+}
+node;
+
+int main(int argc, char *argv[])
+{
+    node *list = NULL;
+}
