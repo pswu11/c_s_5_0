1960b1c23d85467bb0c3966f706c287cb7f3d727
Pei <pswu@protonmail.com>
2023-03-07 11:04:22 +0000

    Tue, Mar 7, 2023, 12:04 PM +01:00

--- wk5/list2.c
+++ wk5/list2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+typedef struct node
 {
     int number;
     struct node *next;
@@ -29,11 +29,11 @@ int main(int argc, char *argv[])
         list = n;
     }
 
-/*     // declare a temp node that will point to a node in the list
+    // declare a temp node that will point to a node in the list
     node *ptr = list;
     while (ptr != NULL)
     {
         printf("%i\n", ptr->number);
         ptr = ptr->next; // borrow the next of the current node and follow through
-    } */
+    }
 }
