cee0efdb97d23f346f6ddd78b401e7578b311264
Pei <pswu@protonmail.com>
2023-03-07 10:44:15 +0000

    Tue, Mar 7, 2023, 11:44 AM +01:00

---
+++ wk5/list2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(void)
+{
+    node *list = NULL;
+}
--- wk5/node.c
+++ wk5/node.c
@@ -1,5 +1,4 @@
 
-
 // initialize node to NULL to make sure there's no garbage value there.
 node *list = NULL;
 node *n = malloc(sizeof(node));
