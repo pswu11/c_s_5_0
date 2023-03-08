04c5e1cc4d44a4fac56f14bce8e907a1de318090
Pei <pswu@protonmail.com>
2023-03-08 11:24:59 +0000

    Wed, Mar 8, 2023, 12:24 PM +01:00

--- wk5/inheritance/inheritance.c
+++ wk5/inheritance/inheritance.c
@@ -54,9 +54,8 @@ person *create_family(int generations)
         p->parents[0] = parent0;
         p->parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        printf("%c\n", parent0->alleles[0]);
-        // p->alleles[0] = random_allele_from_parent(parent0->alleles[0], parent0->alleles[1]);
-        // p->alleles[1] = random_allele_from_parent(parent1->alleles[0], parent1->alleles[1]);
+        p->alleles[0] = random_allele_from_parent(parent0->alleles[0], parent0->alleles[1]);
+        p->alleles[1] = random_allele_from_parent(parent1->alleles[0], parent1->alleles[1]);
     }
 
     // If there are no generations left to create
@@ -72,7 +71,7 @@ person *create_family(int generations)
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return p;
 }
 
 // Free `p` and all ancestors of `p`.
