7135bfe6761a279d28fd71019da85b2c7acdf023
Pei <pswu@protonmail.com>
2023-03-08 10:29:29 +0000

    Wed, Mar 8, 2023, 11:29 AM +01:00

--- wk5/inheritance/inheritance.c
+++ wk5/inheritance/inheritance.c
@@ -53,6 +53,7 @@ person *create_family(int generations)
         p->parents[0] = parent0;
         p->parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        
 
     }
 
