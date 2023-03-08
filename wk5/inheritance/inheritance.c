d69a0e0da2938eafc953588fbc3b9b2b30897755
Pei <pswu@protonmail.com>
2023-03-08 11:13:27 +0000

    Wed, Mar 8, 2023, 12:13 PM +01:00

--- wk5/inheritance/inheritance.c
+++ wk5/inheritance/inheritance.c
@@ -78,9 +78,12 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-
+    if (p == NULL)
+    {
+        return;
+    }
     // TODO: Free parents recursively
-
+    
     // TODO: Free child
 
 }
