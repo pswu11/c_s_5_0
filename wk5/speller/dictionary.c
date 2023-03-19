c8e2616c01b07f449395d0f7d20fb53e9b9f6aed
Pei <pswu@protonmail.com>
2023-03-19 18:06:16 +0000

    Sun, Mar 19, 2023, 7:06 PM +01:00

--- wk5/speller/dictionary.c
+++ wk5/speller/dictionary.c
@@ -34,7 +34,7 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    printf("%i\n", toupper(word[0]) - 'A');
+    printf("%c: %i\n", word[0], toupper(word[0]) - 'A');
     return toupper(word[0]) - 'A';
 }
 
