4c4403ad68a6f79f96116a710200fb7216622059
Pei <pswu@protonmail.com>
2023-03-20 20:28:45 +0000

    Mon, Mar 20, 2023, 9:28 PM +01:00

--- wk6/hello.py
+++ wk6/hello.py
@@ -8,7 +8,14 @@ print(f"Hello, {answer}!") # format string in Python
 # data types: bool, float, int, str,
 # no double or long as int and float already use larger range for them by default.
 
-# conditionals
+# conditionals in Python
 
-if x < y:
+if x > y:
     print("x is greater than y")
+elif x < y:
+    print("x is less than y")
+else:
+    print("x is equal to y")
+
+# variables in Python
+
