108c6ece9e6dc3c85e972fabfbe211eb0ef36272
Pei <pswu@protonmail.com>
2023-03-20 20:15:11 +0000

    Mon, Mar 20, 2023, 9:15 PM +01:00

--- wk6/dictionary.py
+++ wk6/dictionary.py
@@ -1,6 +1,8 @@
 words = set()
-# dict() this will give you a hash table.
-# set() this will give you a hash table without duplicates
+"""
+dict() this will give you a hash table.
+set() this will give you a hash table without duplicates
+"""
 def check(word):
     if word.lower() in words:
         return True
@@ -8,13 +10,21 @@ def check(word):
         return False
 
 def load(dictionary):
-    ...
+    file = open(dictionary, "r")
+    for line in file:
+        # this removes trailing charactors from the line
+        word = line.rstrip()
+        # this adds word to our set
+        words.add(word)
+    close(file)
+    return True
 
 
 def size():
-    ...
+    return len(words)
 
 
+# no need to do memory management in Python
 def unload():
-    ...
+    return True
 
