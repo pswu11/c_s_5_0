c6184f1e29784055bf98a59244d1c40ad837895f
Pei <pswu@protonmail.com>
2023-03-21 21:29:32 +0000

    Tue, Mar 21, 2023, 10:29 PM +01:00

--- wk6/pset6/sentimental-mario-more/mario.py
+++ wk6/pset6/sentimental-mario-more/mario.py
@@ -3,21 +3,25 @@
 from cs50 import get_int
 
 
+# main fuction
 def main():
     while True:
         h = get_int("Height: ")
+        """validate user input"""
         if h >= 1 and h <= 8:
             print_pyramid(h)
             break
 
 
-# print double-pyramid based on height as input
+# print double-pyramid based on height as argument
 def print_pyramid(height):
     for i in range(height):
         i += 1
+        """print the left pyramid"""
         print(" " * (height - i), end="")
         print("#" * i, end="")
         print(" ", end="")
+        """print the right pyramid"""
         print("#" * i, end="")
         print(" " * (height - i))
 
