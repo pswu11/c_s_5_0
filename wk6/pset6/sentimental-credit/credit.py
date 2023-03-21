a820179a0afcd990ea4b4f9b2d159ff819705f89
Pei <pswu@protonmail.com>
2023-03-21 21:39:03 +0000

    Tue, Mar 21, 2023, 10:39 PM +01:00

--- wk6/pset6/sentimental-credit/credit.py
+++ wk6/pset6/sentimental-credit/credit.py
@@ -1,2 +1,14 @@
 # implement credit card number validator
 
+from cs50 import get_int
+
+
+def main():
+    while True:
+        n = get_int("Number: ")
+        """validate user input"""
+        if isinstance(n, int):
+            validate(n)
+            break
+
+def validate(number):
