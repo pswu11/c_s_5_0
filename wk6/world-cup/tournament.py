deb3e027743a3abd73eb269a15e752ea3c61ef5a
Pei <pswu@protonmail.com>
2023-03-21 19:35:04 +0000

    Tue, Mar 21, 2023, 8:35 PM +01:00

--- wk6/world-cup/tournament.py
+++ wk6/world-cup/tournament.py
@@ -59,7 +59,8 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    
+    simulate_round(teams)
+
 
 
 if __name__ == "__main__":
