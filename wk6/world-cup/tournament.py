cf43b0489de4228d45a4d110160991974e9617cd
Pei <pswu@protonmail.com>
2023-03-21 19:51:24 +0000

    Tue, Mar 21, 2023, 8:51 PM +01:00

--- wk6/world-cup/tournament.py
+++ wk6/world-cup/tournament.py
@@ -20,7 +20,7 @@ def main():
         with open(sys.argv[1]) as file:
             reader = csv.DictReader(file)
             for item in reader:
-                teams.append(item[0])
+                teams.append({"team": item[team], "rating": int(item[rating])})
                 print(item)
         print(type(teams[0]))
         file.close()
