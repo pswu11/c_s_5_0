d6bb23c190e8771f34b04a4b9525f16244b7e1a2
Pei <pswu@protonmail.com>
2023-03-21 20:04:49 +0000

    Tue, Mar 21, 2023, 9:04 PM +01:00

--- wk6/world-cup/tournament.py
+++ wk6/world-cup/tournament.py
@@ -31,7 +31,9 @@ def main():
     # Simulate N tournaments and keep track of win counts
     while N > 0:
         winner = simulate_tournament(teams)[0]
-        print(type(winner))
+        team_name = winner["name"]
+        print(winner)
+        print(team_name)
         if winner in counts:
             counts[winner] += 1
         else:
