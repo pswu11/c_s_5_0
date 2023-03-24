43878f53c749c121ba97a4a51692a65efb330860
Pei <pswu@protonmail.com>
2023-03-24 16:59:00 +0000

    Fri, Mar 24, 2023, 4:59 PM +00:00

--- wk6/pset6/sentimental-credit/credit.py
+++ wk6/pset6/sentimental-credit/credit.py
@@ -23,8 +23,11 @@ def luhn_algo(li):
     sum = 0
     for i in range(len(li)):
         if i % 2 == 0:
+            print(li[-i])
             sum += li[-i] * 2
             print(sum)
-
+        else:
+            print(li[-i])
+            sum += li[-i]
 
 main()
