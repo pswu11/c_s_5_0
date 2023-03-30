79e452188e01596f676e90c6650abb358dd56fa7
Pei <pswu@protonmail.com>
2023-03-30 11:45:24 +0000

    Thu, Mar 30, 2023, 1:45 PM +02:00

--- wk7/movies/4.sql
+++ wk7/movies/4.sql
@@ -1,1 +1,3 @@
 -- In 4.sql, write a SQL query to determine the number of movies with an IMDb rating of 10.0.
+
+SELECT count(*) FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE rating = 10.0);
