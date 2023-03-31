bdf079074eb97286071bde12cec297f451de9276
Pei <pswu@protonmail.com>
2023-03-31 07:53:31 +0000

    Fri, Mar 31, 2023, 9:53 AM +02:00

--- wk7/movies/12.sql
+++ wk7/movies/12.sql
@@ -2,6 +2,6 @@
 
 SELECT title FROM movies WHERE id IN (
     SELECT t1.movie_id FROM (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = 'Johnny Depp')) t1
-    LEFT JOIN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = 'Helena Bonham Carter')) t2
+    JOIN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = 'Helena Bonham Carter')) t2
     ON (t1.movie_id = t2.movie_id)
 );
