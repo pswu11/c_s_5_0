42f4bd4a4ffb9de71193a48ab915a47adbb1892c
Pei <pswu@protonmail.com>
2023-03-31 13:17:12 +0000

    Fri, Mar 31, 2023, 3:17 PM +02:00

--- wk7/fiftyville/log.sql
+++ wk7/fiftyville/log.sql
@@ -234,3 +234,15 @@ SELECT receiver FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AN
 | (375) 555-8161 |
 +----------------+
 */
+
+SELECT name FROM people WHERE phone_number IN (SELECT receiver FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration <= 60 AND caller IN (SELECT phone_number FROM people WHERE name = 'Bruce'));
+
+/*
++-------+
+| name  |
++-------+
+| Robin |
++-------+
+*/
+
+SELECT destination_airport_id IN (SELECT phone_number FROM people WHERE name = 'Bruce');
