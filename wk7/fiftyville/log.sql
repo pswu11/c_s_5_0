345af325ef2ba9e054ed9e88b51cecf8e8535639
Pei <pswu@protonmail.com>
2023-03-31 11:16:59 +0000

    Fri, Mar 31, 2023, 1:16 PM +02:00

--- wk7/fiftyville/log.sql
+++ wk7/fiftyville/log.sql
@@ -262,7 +262,7 @@ SELECT passport_number FROM passengers WHERE flight_id IN (SELECT id FROM flight
 
 SELECT DISTINCT p1.name
 FROM people p1
-INNER JOIN (SELECT ) ON s1.person_id = p1.id
+INNER JOIN (SELECT hour, minute, activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute BETWEEN 5 AND 25 AND activity = 'exit';) t1 ON p1.license_plate = 
 INNER JOIN stars s2 ON s2.movie_id = s1.movie_id
 INNER JOIN people p2 ON p2.id = s2.person_id
 WHERE p2.name = 'Kevin Bacon' AND p1.name != 'Kevin Bacon';
