fda373b0619c4c84b116bdb83c05a01248d69919
Pei <pswu@protonmail.com>
2023-10-03 12:44:29 +0000

    Tue, Oct 3, 2023, 2:44 PM +02:00

--- wk9/froshims/app.py
+++ wk9/froshims/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# empty dictionary
+registrants = {}
 
 @app.route("/")
 def index():
@@ -9,4 +11,7 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    return render_template("success.html", name=request.form.get("name"))
+    name = request.form.get("name")
+    sport = reuqest.form.get("sport")
+    registrants[name] = sport
+    return render_template("success.html", name=name)
