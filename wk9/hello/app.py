191929e678203f172e3295260e9278f93f3fe4bb
Pei <pswu@protonmail.com>
2023-10-03 12:28:11 +0000

    Tue, Oct 3, 2023, 2:28 PM +02:00

--- wk9/hello/app.py
+++ wk9/hello/app.py
@@ -34,7 +34,8 @@ def index():
     if request.method == "GET":
         return render_template("index.html")
     elif request.method == "POST":
-        return render_template("greet.html", name=request.args.get("name", "world"))
+        # request.args is only for "GET" request, should use request.form instead
+        return render_template("greet.html", name=request.form.get("name", "world"))
 
 
 """
