372dcf9789e8a16355e46cd0d653b9597240ff83
Pei <pswu@protonmail.com>
2023-10-03 13:21:21 +0000

    Tue, Oct 3, 2023, 3:21 PM +02:00

--- wk9/froshims/app.py
+++ wk9/froshims/app.py
@@ -5,14 +5,19 @@ app = Flask(__name__)
 # empty dictionary
 REGISTRANTS = {}
 
+# available sports
+SPORTS = ["Soccer", "Basketball", "Ultimate Frisbee"]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
     name = request.form.get("name")
     sport = request.form.get("sport")
+        if sport not in SPORTS:
+            return render_template("error.html")
     REGISTRANTS[name] = sport
     return render_template("success.html", name=name, sport=sport)
 
