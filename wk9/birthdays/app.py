d19e6e8e9528d590fa8410d88524ff3484c36303
Pei <pswu@protonmail.com>
2023-10-04 09:08:57 +0000

    Wed, Oct 4, 2023, 11:08 AM +02:00

--- wk9/birthdays/app.py
+++ wk9/birthdays/app.py
@@ -38,6 +38,6 @@ def index():
         # TODO: Display the entries in the database on index.html
         entries = db.execute("SELECT * FROM birthdays")
 
-        return render_template("index.html")
+        return render_template("index.html", entries=entries)
 
 
