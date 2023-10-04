7b49a33a52b0b592134d32939b3cf085af55d283
Pei <pswu@protonmail.com>
2023-10-04 13:39:26 +0000

    Wed, Oct 4, 2023, 3:39 PM +02:00

--- wk4/get.c
+++ wk4/get.c
@@ -4,5 +4,8 @@ int main(void)
 {
     int x;
     printf("x: ");
-    scanf(%i, );
+    // scanf takes a pointer to the variable you want to store the input
+    // in, so you need to use the & operator to get the address of x
+    scanf("%i", &x);
+    printf("user input: %i\n", x);
 }
