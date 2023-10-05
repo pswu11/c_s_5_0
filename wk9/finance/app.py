8be022623b87a92e58ff66b8dfc4cad04e9e98c8
Pei <pswu@protonmail.com>
2023-10-05 10:20:33 +0000

    Thu, Oct 5, 2023, 12:20 PM +02:00

--- wk9/finance/app.py
+++ wk9/finance/app.py
@@ -111,7 +111,13 @@ def register():
     """Register user"""
     if request.method == 'GET':
         return render_template("register.html")
-    return apology("TODO")
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if not username or not password:
+            return apology("TODO")
+        elif:
+            db.execute()
 
 
 @app.route("/sell", methods=["GET", "POST"])
