b2201daa252a7c2390a29fd8ee1fbfcdb099716e
Pei <pswu@protonmail.com>
2023-10-05 10:16:58 +0000

    Thu, Oct 5, 2023, 12:16 PM +02:00

--- wk9/finance/app.py
+++ wk9/finance/app.py
@@ -109,6 +109,8 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
+    if request.method == 'GET':
+        return render_template("register.html")
     return apology("TODO")
 
 
