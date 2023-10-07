33e4d5f6c69df7c41cae5429fcd33b490749c548
Pei <pswu@protonmail.com>
2023-10-07 16:06:06 +0000

    Sat, Oct 7, 2023, 6:06 PM +02:00

--- wk9/finance/app.py
+++ wk9/finance/app.py
@@ -59,10 +59,10 @@ def buy():
             return apology("Symbol and shares must not be blank.", 400)
         if not lookup(symbol):
             return apology("Symbol doesn't exist.", 400)
-        if isinstance(shares, str) and not int(shares):
+        if isinstance(shares, str) and not float(shares):
             return apology("Shares must be integer.", 400)
-        if isinstance(shares, int) and int(shares) <= 0:
-            return apology("Shares must be greater than 0.", 400)
+        if not float(shares).is_integer() or float(shares) <= 0:
+            return apology("Shares must be greater than 0 and not fractional.", 400)
 
         unit_price = lookup(symbol)['price']
         shares = int(shares)
