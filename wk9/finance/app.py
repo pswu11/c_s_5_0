1368b9b861626aad0f4298c681a3d73e20a96a68
Pei <pswu@protonmail.com>
2023-10-07 14:20:58 +0000

    Sat, Oct 7, 2023, 4:20 PM +02:00

--- wk9/finance/app.py
+++ wk9/finance/app.py
@@ -63,7 +63,7 @@ def buy():
         print(unit_price)
         if shares * unit_price > cash:
             return apology("You don't have enough cash", 403)
-        db.execute("INSERT INTO transactions (user, symbol, shares, unit_price, total_price) VALUES(?, ?, ?, ?, ?)", uid, symbol, shares, unit_price, round(unit_price * shares, 2))
+        db.execute("INSERT INTO transactions (user, symbol, shares, unit_price, total_price) VALUES(?, ?, ?, ?, ?)", uid, symbol, shares, unit_price, -round(unit_price * shares, 2))
         current_balance = db.execute("SELECT balance FROM user_balance WHERE user = ? AND symbol = ?", uid, symbol)
         if not current_balance:
             db.execute("INSERT INTO user_balance (user, symbol, balance) VALUES(?, ?, ?)", uid, symbol, shares)
