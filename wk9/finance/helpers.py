1814e1114777d9576e9d820d363c473a034b570e
Pei <pswu@protonmail.com>
2023-10-08 09:10:20 +0000

    Sun, Oct 8, 2023, 11:10 AM +02:00

--- wk9/finance/helpers.py
+++ wk9/finance/helpers.py
@@ -44,14 +44,6 @@ def lookup(symbol):
 
     # Prepare API request
     symbol = symbol.upper()
-
-    if (symbol == "AAAA"):
-        return {"name": "Stock A", "price": 28.00, "symbol": "AAAA"}
-    elif (symbol == "BBBB"):
-        return {"name": "Stock B", "price": 14.00, "symbol": "BBBB"}
-    elif (symbol == "CCCC"):
-        return {"name": "Stock C", "price": 2000.00, "symbol": "CCCC"}
-
     end = datetime.datetime.now(pytz.timezone("US/Eastern"))
     start = end - datetime.timedelta(days=7)
 
